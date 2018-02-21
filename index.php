ec396e528ce1b27c1c3f408c8982a453c7c980e5
Peder Schuller <peder.schuller@gmail.com>
2018-02-21 17:44:54 -0600

    Push current site; initial

---
+++ index.php
@@ -0,0 +1,97 @@
+<?php
+	session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset='charset=utf-8'>
+	<link type="text/css" rel="stylesheet" href="style.css">
+	<link type="image/gif" rel="icon" href="images/OriginsCover.jpg">
+	<title>Trigger Armada</title>
+	<meta name="description" content="Trigger Armada - Home">
+	<meta name="author" content="Peder Schuller">
+</head>
+<body>
+<div class="header">
+	<img src="images/profile2.jpg" alt="Trigger Armada" style="max-width: 100%; height: auto;">
+	<table class="navbuttontable">
+		<tr>
+			<th><a class="two" href="index.php"><h2>Home</h2></a></a></th>
+			<th><a class="two" href="index.php"><h2>Shows</h2>(coming soon)</a></th>
+			<th><a class="two" href="index.php"><h2>Music</h2>(coming soon)</a></th>
+			<th><a class="two" href="index.php"><h2>Merch</h2>(coming soon)</a></th>
+			<th><a class="two" href="index.php"><h2>Media</h2>(coming soon)</a></th>
+			<th><a class="two" href="index.php"><h2>About</h2>(coming soon)</a></th>
+			<th><a class="two" href="index.php"><h2>Contact</h2>(coming soon)</a></th>
+		</tr>
+	</table>
+</div>
+<div class="content">
+	<div id="mainleft">
+		<h1>WELCOME TO THE ARMADA! <br />(this website is currently under construction)</h1>
+		<hr>
+		<!-- Dynamic content begin here -->
+		<div class="newssection">
+			<hr>
+			<div class="newsmedia">
+				<img alt="Origins Album Cover" src="images/OriginsCover.jpg" width="50%" height="auto" align="middle">
+			</div>
+			<div class="newsblurbtitle">
+				<h2>Origins Release Date... 10.1.2017</h2>
+			</div>
+		</div>
+		<div class="newssection">
+			<hr>
+			<div class="newsmedia">
+				<iframe width="80%" height="500px" frameBorder="0" src="https://www.youtube.com/embed/87bLz9Hoipc">
+				</iframe>
+			</div>
+			<div class="newsblurbtitle">
+				<h2>Thanks to <a class="one" href="http://www.freedomridgerocks.com/">Freedom Ridge</a> for a great festival!</h2>
+			</div>
+		</div>
+		<div class="newssection">
+			<hr>
+			<div class="newsmedia">
+				<iframe width="80%" height="500px" frameBorder="0" src="https://youtube.com/embed/Cw10PlJzTHQ">
+				</iframe>
+			</div>
+			<div class="newsblurbtitle">
+				<h2>Thanks to <a class="one" href="https://www.keithdrakephotography.com/">Keith Drake Photography</a> for this awesome video of our 06/05/2016 show at the Cabooze!</h2>
+			</div>
+		</div>
+		<!--End Dynamic content -->
+	</div>
+	<div id="mainright">
+		<h1>UPCOMING EVENTS</h1>
+		<hr>
+		<hr>
+		<h3>Trigger Armada does not currently have any shows scheduled, we're writing new music!  <a class="one" href="index.php">Contact us</a> for booking inquiries!</h3>
+		<!-- TODO: Dynamic Content should start here
+		<a class="one" href="https://www.facebook.com/events/1170903636351630/?ref=br_rs">
+		<img alt="Haken 10th Anniversary Poster" src="images/HakenPoster.jpg" width="100%" height="auto">	
+		<h2>9/19/2017 - Haken 10th Anniversary Tour @ The Amsterdam</h2></a>
+		----
+		<hr>
+		<a class="one" href="https://www.facebook.com/events/522436471424366/?ref=br_rs">
+		<img alt="Friday the 13th" src="images/Friday13th.jpg" width="100%" height="auto">
+		<h2>10/13/2017 - Friday the 13th w/ Trigger Armada in Duluth</h2></a>
+		----
+		<hr>
+		<a class="one" href="https://www.facebook.com/events/115830822412468/?ref=br_rs">
+		<img alt="Symphonies of Death Poster" src="images/SymphoniesOfDeathPoster.jpg" width="100%" height="auto">
+		<h2>10/21/2017 - Symphonies of Death @ Club Underground</h2></a>
+		----
+		<hr>
+		<h2>10/27/2017 - Trigger Armada @ The Garage</h2>
+		End Dynamic Content here -->
+	</div>
+</div>
+<div id="bottom-bar">
+	<p>Design and Development by Peder Schuller - Trigger Armada<br />©2017 Trigger Armada.  All rights reserved.</p>
+</div>
+</body>
+
+</html>
