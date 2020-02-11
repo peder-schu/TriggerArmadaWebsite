e3aa50b1cfcb5a6a96a8909e6516ef97a8ee1b56
Peder Schuller <peder.schuller@gmail.com>
2020-02-11 15:53:23 -0600

    Update clean header with overlays

--- index.php
+++ index.php
@@ -12,35 +12,39 @@
 </head>
 <body>
 <div class="header">
-	<img src="images/profile2.jpg" alt="Trigger Armada" style="max-width: 100%; height: auto;">
-	<table class="navbuttontable">
-		<tr>
-			<th><a class="two" href="index.php"><h2>Home</h2></a></a></th>
-			<th><a class="two" href="events.php"><h2>Shows</h2></a></th>
-			<th><a class="two" href="music.php"><h2>Music</h2></a></th>
-			<th><a class="two" href="merch.php"><h2>Merch</h2></a></th>
-			<th><a class="two" href="about.php"><h2>About</h2></a></th>
-			<th><a class="two" href="contact.php"><h2>Contact</h2></a></th>
-		</tr>
-	</table>
-</div>
-<div class="header">
-	<table class="navbuttontable">
-		<tr>
-			<th><a href="https://www.facebook.com/TriggerArmada/" target="_blank">
-					<img src="images/FB-f-Logo__white_29.png" alt="Facebook" style="max-width: 100%; height: auto;"></a></th>
-			<th><a href="https://www.youtube.com/channel/UCDvGS1LygBKE5eOWaqdTBLw" target="_blank">
-					<img src="images/yt_icon_mono_dark.png" alt="YouTube" style="height: 29px; max-width: auto;"></a></th>
-			<th><a href="https://open.spotify.com/artist/62HJgVLPeoYT27dfPCDsXh" target="_blank">
-					<img src="images/Spotify_Icon_CMYK_White.png" alt="Spotify" style="height: 29px; max-width: auto;"></a></th>
-			<th><a href="https://itunes.apple.com/us/artist/trigger-armada/1294937470" target="_blank">
-					<img src="images/Apple_Music_Icon_wht.svg" alt="Apple Music" style="height: 29px; max-width: auto;"></a></th>
-			<th><a href="https://www.instagram.com/triggerarmada/" target="_blank">
-					<img src="images/IG_Glyph_Fill.png" alt="Instagram" style="height: 37px; max-width: auto;"></a></th>
-			<th><a href="https://twitter.com/TriggerArmada" target="_blank">
-					<img src="images/Twitter_Logo_Fill.png" alt="Twitter" style="height: 30px; max-width: auto;"></a></th>
-		</tr>
-	</table>
+	<img src="images/profile2.jpg" alt="Trigger Armada" style="width: 100%;">
+	<div class="socialmedialinks">
+		<table class="socialmedialinktable">
+			<tr>
+				<th class="sm"><a href="https://www.facebook.com/TriggerArmada/" target="_blank">
+						<img src="images/FB-f-Logo__white_29.png" alt="Facebook" style="max-width: 100%; height: auto;"></a></th>
+				<th class="sm"><a href="https://www.youtube.com/channel/UCDvGS1LygBKE5eOWaqdTBLw" target="_blank">
+						<img src="images/yt_icon_mono_dark.png" alt="YouTube" style="height: 29px; max-width: auto;"></a></th>
+				<th class="sm"><a href="https://open.spotify.com/artist/62HJgVLPeoYT27dfPCDsXh" target="_blank">
+						<img src="images/Spotify_Icon_CMYK_White.png" alt="Spotify" style="height: 29px; max-width: auto;"></a></th>
+			</tr>
+			<tr>
+				<th class="sm"><a href="https://itunes.apple.com/us/artist/trigger-armada/1294937470" target="_blank">
+						<img src="images/Apple_Music_Icon_wht.svg" alt="Apple Music" style="height: 29px; max-width: auto;"></a></th>
+				<th class="sm"><a href="https://www.instagram.com/triggerarmada/" target="_blank">
+						<img src="images/IG_Glyph_Fill.png" alt="Instagram" style="height: 37px; max-width: auto;"></a></th>
+				<th class="sm"><a href="https://twitter.com/TriggerArmada" target="_blank">
+						<img src="images/Twitter_Logo_transparent.png" alt="Twitter" style="height: 30px; max-width: auto;"></a></th>
+			</tr>
+		</table>
+	</div>
+	<div class="navbar">
+		<table class="navbuttontable">
+			<tr>
+				<th><a class="two" href="index.php"><h2>Home</h2></a></a></th>
+				<th><a class="two" href="events.php"><h2>Shows</h2></a></th>
+				<th><a class="two" href="music.php"><h2>Music</h2></a></th>
+				<th><a class="two" href="merch.php"><h2>Merch</h2></a></th>
+				<th><a class="two" href="about.php"><h2>About</h2></a></th>
+				<th><a class="two" href="contact.php"><h2>Contact</h2></a></th>
+			</tr>
+		</table>
+	</div>
 </div>
 <div class="content">
 	<div id="mainleft">
@@ -118,7 +122,7 @@
 	</div>
 </div>
 <div id="bottom-bar">
-	<p>Design and Development by Peder Schuller - Trigger Armada<br />©2017 Trigger Armada.  All rights reserved. <!--<a href="login.php">Administrator</a>-->
+	<p>Design and Development by Peder Schuller - Trigger Armada<br />©2020 Trigger Armada.  All rights reserved. <!--<a href="login.php">Administrator</a>-->
 	</p>
 </div>
 </body>
